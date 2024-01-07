ace71d7809c28ddd90e771f6d5ca0a1c3d003dfb
MoeFingers <mbzuiter@gmail.com>
2024-01-06 18:35:31 -0700

    functionality for map generation based on array complete

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -1,20 +1,21 @@
 //define block size for everything
-let fullEdgeSizeStyle = {"length":96, "girth":10}
+let fullEdgeSize = {"length":96, "girth":10}
+let fullControlSize = 100
 
 // making some classes for types of players
-let halfEdgeStartStyleGreen = `width:${fullEdgeSizeStyle.length/2}px;height:${fullEdgeSizeStyle.girth}px;background:green;`
-let halfEdgeEndStyleGreen = `width:${fullEdgeSizeStyle.length/2}px;height:${fullEdgeSizeStyle.girth}px;background:green;`
+let halfEdgeStartStyleGreen = `width:${fullEdgeSize.length/2}px;height:${fullEdgeSize.girth}px;background:green;`
+let halfEdgeEndStyleGreen = `width:${fullEdgeSize.length/2}px;height:${fullEdgeSize.girth}px;background:green;`
 
-let halfEdgeStartStyleRed = `width:${fullEdgeSizeStyle.length/2}px;height:${fullEdgeSizeStyle.girth}px;background:red;`
-let halfEdgeEndStyleRed = `width:${fullEdgeSizeStyle.length/2}px;height:${fullEdgeSizeStyle.girth}px;background:red;`
+let halfEdgeStartStyleRed = `width:${fullEdgeSize.length/2}px;height:${fullEdgeSize.girth}px;background:red;`
+let halfEdgeEndStyleRed = `width:${fullEdgeSize.length/2}px;height:${fullEdgeSize.girth}px;background:red;`
 
 
 // map
 let map1 = [
   [0, 1, 1, 1, 1, 0],
-  [1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 0, 1],
+  [1, 1, 0, 1, 1, 1],
+  [1, 1, 0, 0, 1, 1],
   [1, 1, 1, 1, 1, 1],
   [0, 1, 1, 1, 1, 0], //using numbers instead of bool for writability
 ];
@@ -22,16 +23,34 @@ let map1 = [
 //load map
 function loadMap(map){ //accept map as parameter when invoking
     for(let selectedRow = 0; selectedRow < map.length; selectedRow++){ //initialize row number as 0, select rows while less than map.length, etc.
-        let currentRow = (map[selectedRow]) //select row from map (for readability)
-        for(let selectedBlock = 0; selectedBlock < currentRow.length; selectedBlock++){// intialize block number as 0, select blocks while less than map.length, etc.
+        let currentRowArray = (map[selectedRow]) //select row from map (for readability)
+        for(let selectedBlock = 0; selectedBlock < currentRowArray.length; selectedBlock++){// intialize block number as 0, select blocks while less than map.length, etc.
             let controlProperties = {
-              active: currentRow[selectedBlock],
+              active: currentRowArray[selectedBlock],
               pseudoCoordinates: [selectedBlock + 1,selectedRow + 1]
             };
+            //console.log(controlProperties.pseudoCoordinates)
+
+            let rightControlProperties = {
+                active: "", // this block will not exist since it is after everything
+                pseudoCoordinates: [selectedBlock + 2,selectedRow + 1]
+            };
+            if(selectedBlock + 1 >= currentRowArray.length ){rightControlProperties.active = 0}
+            else if (selectedBlock < currentRowArray.length) {rightControlProperties.active = currentRowArray[selectedBlock + 1]}
+            console.log(`${rightControlProperties.pseudoCoordinates} active: ${rightControlProperties.active}`)
+
+            let belowControlProperties = {
+            active: "", //this row will not exist since it is after everything
+            pseudoCoordinates: [selectedBlock + 1,selectedRow + 2]
+            };
+            if(selectedRow + 1 >= map.length ){belowControlProperties.active = 0}
+            else if (selectedRow < map.length) {belowControlProperties.active = map[selectedRow + 1][selectedBlock]}
+            console.log(`${belowControlProperties.pseudoCoordinates} active: ${belowControlProperties.active}`)
+
             if (controlProperties.active == true){ // this value will actually be 1, but using boolean for readability
                 console.log(`${controlProperties.pseudoCoordinates} active`)
-                let locationX = selectedBlock * 100
-                let locationY = selectedRow * 100
+                let locationX = selectedBlock * fullControlSize
+                let locationY = selectedRow * fullControlSize
                 // MAKE BLOCK
                 let control = document.createElement("div")
                 control.id = `control-x${controlProperties.pseudoCoordinates[0]}-y${controlProperties.pseudoCoordinates[1]}`
@@ -41,29 +60,68 @@ function loadMap(map){ //accept map as parameter when invoking
                 control.style.translate = `${locationX}px ${locationY}px`
                 document.getElementById("mapArea").append(control)
 
-                // MAKE TOP EDGE
-                let controlTopStart = document.createElement("div")
-                controlTopStart.className = "halfEdgeStart"
-                controlTopStart.style = halfEdgeStartStyleGreen
+                // MAKE HORIZONTAL EDGES (with dynamically named IDs )
+                function horizontalEdge(controlCoordinates, locY){
+                    let controlHorizontalStart = document.createElement("div")
+                    controlHorizontalStart.id = `controlHorizontalStart-x${controlCoordinates[0]}-y${controlCoordinates[1]}`
+                    controlHorizontalStart.className = "halfEdgeStart"
+                    controlHorizontalStart.style = halfEdgeStartStyleGreen
 
-                let controlTopEnd = document.createElement("div")
-                controlTopEnd.className = "halfEdgeEnd"
-                controlTopEnd.style = halfEdgeEndStyleRed
+                    let controlHorizontalEnd = document.createElement("div")
+                    controlHorizontalEnd.id = `controlHorizontalEnd-x${controlCoordinates[0]}-y${controlCoordinates[1]}`
+                    controlHorizontalEnd.className = "halfEdgeEnd"
+                    controlHorizontalEnd.style = halfEdgeEndStyleRed
 
-                let controlTop = document.createElement("div")
-                controlTop.id = `controlTop-x${controlProperties.pseudoCoordinates[0]}-y${controlProperties.pseudoCoordinates[1]}`
-                controlTop.className = "halfEdgeContainer"
-                controlTop.style.width = `${fullEdgeSizeStyle.length}px`
-                controlTop.style.position ="absolute"
-                controlTop.style.transformOrigin = "0px 0px"
-                controlTop.style.translate = `${locationX}px ${locationY -5}px`
+                    let controlHorizontal = document.createElement("div")
+                    controlHorizontal.id = `controlHorizontal-x${controlCoordinates[0]}-y${controlCoordinates[1]}`
+                    controlHorizontal.className = "halfEdgeContainer"
+                    controlHorizontal.style.width = `${fullEdgeSize.length}px`
+                    controlHorizontal.style.position ="absolute"
+                    controlHorizontal.style.transformOrigin = "0px 0px"
+                    controlHorizontal.style.translate = `${locationX}px ${locY -5}px`
 
-                controlTop.append(controlTopStart)
-                controlTop.append(controlTopEnd)
+                    controlHorizontal.append(controlHorizontalStart)
+                    controlHorizontal.append(controlHorizontalEnd)
 
-                document.getElementById("mapArea").append(controlTop)
+                    document.getElementById("mapArea").append(controlHorizontal)
+                }
+                // MAKE VERTICAL EDGES (with dynamically named IDs )
+                function verticalEdge(controlCoordinates, locX){
+                    let controlVerticalStart = document.createElement("div")
+                    controlVerticalStart.id = `controlVerticalStart-x${controlCoordinates[0]}-y${controlCoordinates[1]}`
+                    controlVerticalStart.className = "halfEdgeStart"
+                    controlVerticalStart.style = halfEdgeStartStyleGreen
 
+                    let controlVerticalEnd = document.createElement("div")
+                    controlVerticalEnd.id = `controlVerticalEnd-x${controlCoordinates[0]}-y${controlCoordinates[1]}`
+                    controlVerticalEnd.className = "halfEdgeEnd"
+                    controlVerticalEnd.style = halfEdgeEndStyleRed
 
+                    let controlVertical = document.createElement("div")
+                    controlVertical.id = `controlVertical-x${controlCoordinates[0]}-y${controlCoordinates[1]}`
+                    controlVertical.className = "halfEdgeContainer"
+                    controlVertical.style.width = `${fullEdgeSize.length}px`
+                    controlVertical.style.position ="absolute"
+                    controlVertical.style.transformOrigin = "0px 0px"
+                    controlVertical.style.rotate = "90deg"
+                    controlVertical.style.translate = `${locX+3}px ${locationY +2}px`
+
+                    controlVertical.append(controlVerticalStart)
+                    controlVertical.append(controlVerticalEnd)
+
+                    document.getElementById("mapArea").append(controlVertical)
+                }
+                verticalEdge(controlProperties.pseudoCoordinates, locationX)
+                console.log(rightControlProperties.active)
+                if(rightControlProperties.active == false){
+                    verticalEdge(rightControlProperties.pseudoCoordinates, (locationX + fullControlSize))
+                }
+                horizontalEdge(controlProperties.pseudoCoordinates, locationY)
+                console.log(belowControlProperties.active)
+                if(belowControlProperties.active == false){
+                    horizontalEdge(belowControlProperties.pseudoCoordinates, (locationY + fullControlSize))
+                }
+               
             } else if (controlProperties.active == false){
                 console.log(`${controlProperties.pseudoCoordinates} not active`)
             }
@@ -84,7 +142,7 @@ exampleTopEnd.style = halfEdgeEndStyleRed
 
 let exampleTop = document.createElement("div")
 exampleTop.className = "halfEdgeContainer"
-exampleTop.style.width = `${fullEdgeSizeStyle.length}px`
+exampleTop.style.width = `${fullEdgeSize.length}px`
 exampleTop.style.position ="absolute"
 exampleTop.style.transformOrigin = "0px 0px"
 exampleTop.style.translate = "0px -5px"
@@ -106,7 +164,7 @@ exampleLeftEnd.style = halfEdgeEndStyleRed
 
 let exampleLeft = document.createElement("div")
 exampleLeft.className = "halfEdgeContainer"
-exampleLeft.style.width = `${fullEdgeSizeStyle.length}px`
+exampleLeft.style.width = `${fullEdgeSize.length}px`
 exampleLeft.style.position ="absolute"
 exampleLeft.style.transformOrigin = "0px 0px"
 exampleLeft.style.translate = "3px 2px"
@@ -129,7 +187,7 @@ exampleRightEnd.style = halfEdgeEndStyleRed
 
 let exampleRight = document.createElement("div")
 exampleRight.className = "halfEdgeContainer"
-exampleRight.style.width = `${fullEdgeSizeStyle.length}px`
+exampleRight.style.width = `${fullEdgeSize.length}px`
 exampleRight.style.position ="absolute"
 exampleRight.style.transformOrigin = "0 0"
 exampleRight.style.rotate = "90deg"
@@ -152,7 +210,7 @@ exampleBottomEnd.style = halfEdgeEndStyleRed
 
 let exampleBottom = document.createElement("div")
 exampleBottom.className = "halfEdgeContainer"
-exampleBottom.style.width = `${fullEdgeSizeStyle.length}px`
+exampleBottom.style.width = `${fullEdgeSize.length}px`
 exampleBottom.style.position ="absolute"
 exampleBottom.style.transformOrigin = "0px 0px"
 exampleBottom.style.translate = "0 95px"
