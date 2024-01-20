17261368e8f5634e0fe66f6a6576af257c8484db
MoeFingers <mbzuiter@gmail.com>
2024-01-20 12:06:32 -0800

    everything working with gameover page

--- assets/js/loadMapSelector.js
+++ assets/js/loadMapSelector.js
@@ -244,7 +244,7 @@ export async function loadMapSelector(gameContainer){
             scoreBoardElementsArray.push(generateScoreBoards(count, characterColorArray[count], gameContainer));
             playerObject[count] = characterColorArray[count]
           }
-          generateTimer(5,innerGameContainer,playerObject, scoreBoardElementsArray)      
+          generateTimer(80,innerGameContainer,playerObject, scoreBoardElementsArray)      
         }, false)
 
     gameContainer.append(playGameButton)
